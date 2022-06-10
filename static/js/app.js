d1d9e74720f234a58a6ab40e535dd545efae625e
LeoTovell <51256511+LeoTovell@users.noreply.github.com>
2022-06-10 10:16:39 +0100

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,22 @@
 
 
-var init = function(){
+function init(){
 	console.log("Starting graph program.")
 
 	// CANVAS CREATION
 
-	var leftDiv = document.getElementById("right-div");
+	var rightDiv = document.getElementById("right-div");
 	// var canvas = document.createElement('canvas');
 
-	console.log(leftDiv.offsetWidth)
+	console.log(rightDiv.offsetWidth)
 
 	var canvas = document.createElement("canvas");
 
 	canvas.id = "canvas";
-	canvas.width = leftDiv.offsetWidth;
-	canvas.height = leftDiv.offsetHeight;
+	canvas.width = rightDiv.offsetWidth;
+	canvas.height = rightDiv.offsetHeight;
 	
-	leftDiv.appendChild(canvas);
+	rightDiv.appendChild(canvas);
 
 	var canvas = document.getElementById("canvas");
 	var gl = canvas.getContext("2d");
@@ -37,11 +37,33 @@ if(!gl){
 	// Each iteration get new positions, or if the list is unchanged, skip over.
 
 	gl.beginPath();
-	gl.arc(300, 250, 200, 0, 2 * Math.PI);
+	gl.moveTo(300, 200);
+	gl.lineTo(500, 400);
 	gl.stroke();
-};
+	gl.closePath();
+
+	gl.beginPath();
+	gl.arc(300, 200, 25, 0, 2 * Math.PI); // X, Y, radius
+	gl.fillStyle = "white"
+	gl.fill()
+	gl.stroke();
+	gl.closePath();
+
+	gl.font = "20px serif";
+	gl.strokeText("A", 295, 207)
 
 
-var hideButton = function(){
-	document.getElementById("initButton").hidden = true;
+	gl.beginPath();
+	gl.arc(500, 400, 25, 0, 2 * Math.PI); // X, Y, radius
+	gl.fillStyle = "white"
+	gl.fill()
+	gl.stroke();
+	gl.closePath();
+
+	gl.strokeText("B", 495, 408)
+}
+
+
+function hideButton(){
+	document.getElementById("initButtonDiv").style.display = "None"
 }
