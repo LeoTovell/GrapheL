65dd7c5d1dbac3057a1377f8a848ea5a2d8d49f8
LeoTovell <51256511+LeoTovell@users.noreply.github.com>
2022-06-16 12:30:50 +0100

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,6 @@
-var gl;
-var graph = [];
-
-console.log(socket);
-
-socket.on("test_event", function(data){
-	console.log("test data");
-})
-
-socket.on("graph", ({vertices, edges}) => {
-	graph = [vertices, edges]
-	console.log("Graph recieved");
-})
-
-function request_graph(){
-	socket.emit("request_graph");
-}
+var ctx;
+var running = true;
+var x = y = 200;
 
 function mousemove(canvas, event){
 	var rect = canvas.getBoundingClientRect();
@@ -36,120 +22,83 @@ function init(){
 	console.log("Starting graph program.")
 	document.getElementById("initButton").remove()
 
-	// CANVAS CREATION
-
-	// var rightDiv = document.getElementById("canvas-container");
-	// // var canvas = document.createElement('canvas');
-
-	// console.log(rightDiv.offsetWidth)
-	// console.log(rightDiv.offsetHeight)
-
-	
-	
-	// rightDiv.appendChild(canvas);
+	// CREATE Canvas
 
 
-	// CREATE UI
-
-	// var app_container = document.createElement("div");
-	var app_container = document.getElementById("app_container")
-	var control_div = document.createElement("div");
-	var canvas_div = document.createElement("div");
+	var app_container = document.getElementById("app-container")
 	var canvas = document.createElement("canvas");
+	var canvas_div = document.getElementById("canvas-div");
 
 	canvas.id = "canvas";
 	canvas.setAttribute("onmousemove", "mousemove(this, event)")
-
-	control_div.id = "controls-container";
-	canvas_div.id = "canvas-container";
-	// app_container.id = "page-container";
-
-	// app_container.style = "padding: 20px; display: flex; align-items: stretch; height: 100%; position: relative"
-	control_div.style = "float:left; border: 2px solid red; position: relative; height: 100%; width: 18%";
-	canvas_div.style = "float:right; border: 2px solid green; margin-left: 10px; position: relative; height: 100%; width: 78%";
-
-
-	app_container.appendChild(control_div);
-	app_container.appendChild(canvas_div);
-	document.body.appendChild(app_container);
-
 	canvas.width = canvas_div.offsetWidth;
 	canvas.height = canvas_div.offsetHeight;
 	canvas_div.appendChild(canvas)
 
-	gl = canvas.getContext("2d");
+	ctx = canvas.getContext("2d");
 
-	if (!gl){
-		console.log("Your browser does not support webGL without falling back on experimental.")
-		gl = canvas.getContext('experimental-webgl');
-		}
+if (!ctx){
+	console.log("Your browser does not support webctx without falling back on experimental.")
+	ctx = canvas.getContext('experimental-webctx');
+	}
 
-	if(!gl){
-		alert("Your browser does not support webGL")
-		}
+if(!ctx){
+	alert("Your browser does not support webctx")
+	}
 
-	// gl.clearColor(0.75, 0.85, 0.8, 1.0);
-	// gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	// ctx.clearColor(0.75, 0.85, 0.8, 1.0);
+	// ctx.clear(ctx.COLOR_BUFFER_BIT | ctx.DEPTH_BUFFER_BIT);
 
 	// Begin Loop
 	// Each iteration get new positions, or if the list is unchanged, skip over.
 
-	graph = request_graph();
+	ctx.beginPath();
+	ctx.moveTo(300, 200);
+	ctx.lineTo(500, 400);
+	ctx.stroke();
+	ctx.closePath();
 
-	// while(graph == null){
-		// console.log("waiting")
-	// }
-}
+	ctx.beginPath();
+	ctx.arc(300, 200, 25, 0, 2 * Math.PI); // X, Y, radius
+	ctx.fillStyle = "white"
+	ctx.fill()
+	ctx.stroke();
+	ctx.closePath();
 
-function updateGraphAttribute(attribute, value){
-	socket.emit("updateGraphAttribute", {attribute, value});
-}
-
-function draw(){
-
-	vertices = graph[0]
-	edges = graph[1];
-
-	for( let i = 0; i < edges.length; i++){
-		edge = edges[i]
-		gl.beginPath();
-		gl.moveTo(edge[0], edge[1]);
-		gl.lineTo(edge[2], edge[3]);
-		gl.stroke();
-		gl.closePath();
-	};
-
-	// for(vertex in vertices)
-
-	gl.beginPath();
-	gl.moveTo(300, 200);
-	gl.lineTo(500, 400);
-	gl.stroke();
-	gl.closePath();
-
-	gl.beginPath();
-	gl.arc(300, 200, 25, 0, 2 * Math.PI); // X, Y, radius
-	gl.fillStyle = "white"
-	gl.fill()
-	gl.stroke();
-	gl.closePath();
-
-	gl.font = "20px serif";
-	gl.strokeText("A", 293, 207)
+	ctx.font = "20px serif";
+	ctx.strokeText("A", 293, 207)
 
 
-	gl.beginPath();
-	gl.arc(500, 400, 25, 0, 2 * Math.PI); // X, Y, radius
-	gl.fillStyle = "white"
-	gl.fill()
-	gl.stroke();
-	gl.closePath();
+	ctx.beginPath();
+	ctx.arc(500, 400, 25, 0, 2 * Math.PI); // X, Y, radius
+	ctx.fillStyle = "white"
+	ctx.fill()
+	ctx.stroke();
+	ctx.closePath();
 
-	gl.strokeText("B", 495, 408)
+	ctx.strokeText("B", 495, 408)
+
 
 }
 
+function canvas_draw_square(){
+	ctx.beginPath();
+	ctx.rect(x, y, 200, 200);
+	ctx.fillStyle = "black";
+	ctx.fill()
+	ctx.stroke();
+	ctx.closePath();
+}
+
+function canvas_clear(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 function hideButton(){
 	document.getElementById("initButtonDiv").style.display = "None"
+}
+
+function incrementPos(num){
+	x += num;
+	y += num;
 }
